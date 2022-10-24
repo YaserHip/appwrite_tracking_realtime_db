d1a6756c06dd230cb3f43eba056d8e6851284929
Yaser <jonathanhipolito@MacBook-Air-de-Yaser.local>
2022-10-24 16:20:53 -0600

    page home ui changes

--- lib/app/features/home/page_home.dart
+++ lib/app/features/home/page_home.dart
@@ -1,4 +1,5 @@
 import 'package:appwrite_tracking_realtime_db/app/features/home/controller_home.dart';
+import 'package:appwrite_tracking_realtime_db/app/features/models/model_location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,17 +9,26 @@ class PageHome extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final streamLocationProvider = ref.watch(providerStreamLocationFromDB);
+    final streamLocationListener = ref.watch(providerStreamLocation);
     return Scaffold(
         body: Center(
       child: Container(
-        child: streamLocationProvider.when(data: (data) {
+          child: Column(
+        children: [
+          containerLatLonInformation(streamLocationProvider),
+        ],
+      )),
+    ));
+  }
+
+  containerLatLonInformation(AsyncValue<ModelLocation> stream) => Center(
+          child: Container(
+        child: stream.when(data: (data) {
           return Text('lat: ${data.lat}, lon: ${data.lon}');
         }, error: (e, r) {
           return Text(e.toString());
         }, loading: () {
           return const CircularProgressIndicator();
         }),
-      ),
-    ));
-  }
+      ));
 }
